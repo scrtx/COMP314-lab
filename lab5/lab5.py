8b62c7d147878f21b28afb678a377ac24514503f
Sankalpa <snksatyal@gmail.com>
2023-06-04 23:42:52 +0545

    Lab 5 final

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -2,18 +2,19 @@ from lab5graph import makeGraph, Graph, libGraph
 from prettytable import PrettyTable
 
 aves = ('data/aves-sparrow-social.edges', ['a', 'b', 'w', 'year'])
-bio = ('data/bio-DM-LC.edges', ['a', 'b', 'w'])
-email = ('data/email-univ.edges', ['a', 'b'])
-reptilia = ('data/reptilia-tortoise-network-fi.edges', ['a', 'b', 'year'])
-road = ('data/road-euroroad.edges', ['a', 'b'])
-rt = ('data/rt_voteonedirection.edges', ['a', 'b', 'w'])
+rt_barahin = ('data/rt_bahrain.edges', ['a', 'b', 'w'])
+rt_dash = ('data/rt_dash.edges', ['a', 'b', 'w'])
+rt_gop = ('data/rt_gop.edges', ['a', 'b', 'w'])
+rt_oman = ('data/rt_oman.edges', ['a', 'b', 'w'])
+soc = ('data/soc-advogato.edges', ['a', 'b', 'w'])
+
 
 G1 = makeGraph(aves[0], aves[1])
-G2 = makeGraph(bio[0], bio[1])
-G3 = makeGraph(email[0], email[1])
-G4 = makeGraph(reptilia[0], reptilia[1])
-G5 = makeGraph(road[0], road[1])
-G6 = makeGraph(rt[0], rt[1])
+G2 = makeGraph(rt_barahin[0], rt_barahin[1])
+G3 = makeGraph(rt_dash[0], rt_dash[1])
+G4 = makeGraph(rt_gop[0], rt_gop[1])
+G5 = makeGraph(rt_oman[0], rt_oman[1])
+G6 = makeGraph(soc[0], soc[1])
 
 G = [Graph(G1), Graph(G2), Graph(G3), Graph(G4) ,Graph(G5), Graph(G6)]
 H = [libGraph(G1), libGraph(G2), libGraph(G3), libGraph(G4), libGraph(G5), libGraph(G6)]
